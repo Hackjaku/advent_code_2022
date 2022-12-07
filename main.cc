ef9b67e1c6cf03136d0ae8139d5db07ac4e53819
Hackjaku <stressfest@rocketmail.com>
2022-12-07 18:07:47 +0100

    UPDATE: puzzle 7

--- main.cc
+++ main.cc
@@ -9,13 +9,38 @@ using namespace std;
 struct Directory {
     string name;
     int files_size;
-    vector<Directory> children;
+    vector<Directory*> children;
+    Directory* parent;
+
+    Directory(string name, Directory* parent) {
+        this->name = name;
+        files_size = 0;
+        this->parent = parent;
+    }
+
+    void add_child(string name) {
+        Directory* child = new Directory(name, this);
+        children.push_back(child);
+    }
+
+    bool child_exists(string name) {
+        for (Directory* child : children) {
+            if (child->name == name) {
+                return true;
+            }
+        }
+        return false;
+    }
 };
+
+
 
 int main() {
     fstream data("../inputs/7.txt", ios::in);
     
     string line;
+
+    Directory* root = new Directory("/", nullptr);
 
     while(getline(data, line)) {
         std::istringstream iss(line);
@@ -23,6 +48,12 @@ int main() {
 
         if (!(iss >> s)) { 
             break;
+        }
+
+        if (s == "$") {
+            // command
+        } else {
+            // directory
         }
 
         // do stuff
