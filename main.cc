3eed2538d776c0a4c8cef7cf26a31bb2de3642d8
Hackjaku <stressfest@rocketmail.com>
2022-12-13 18:10:31 +0100

    DONE: 7_2

--- main.cc
+++ main.cc
@@ -2,141 +2,28 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <vector>
 
 using namespace std;
 
-struct Directory {
-    string name;
-    int files_size;
-    vector<Directory*> children;
-    Directory* parent;
-
-    int total_size;
-
-    Directory(string name, Directory* parent) {
-        this->name = name;
-        files_size = 0;
-        this->parent = parent;
-    }
-
-    void add_child(string name) {
-        if (child_exists(name)) {
-            return;
-        }
-        Directory* child = new Directory(name, this);
-        children.push_back(child);
-    }
-
-    bool child_exists(string name) {
-        for (Directory* child : children) {
-            if (child->name == name) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void add_file(int size) {
-        files_size += size;
-    }
-
-    Directory* get_child(string name) {
-        for (Directory* child : children) {
-            if (child->name == name) {
-                return child;
-            }
-        }
-        return nullptr;
-    }
-
-    int set_total_size() {
-        int total = files_size;
-        for (Directory* child : children) {
-            total += child->set_total_size();
-        }
-        total_size = total;
-        return total;
-    }
-
-    void add_size(vector<int>& sizes) {
-        if (total_size <= 100000) {
-            sizes.push_back(total_size);
-        }
-        for (Directory* child : children) {
-            child->add_size(sizes);
-        }
-    }
-};
-
-
+#define MAZE_HEIGHT 41
+#define MAZE_WIDTH 161
 
 int main() {
-    fstream data("../inputs/7.txt", ios::in);
+    fstream data("../inputs/12.txt", ios::in);
     
     string line;
 
-    Directory* root = new Directory("/", nullptr);
-    Directory* current_dir = root;
-
-    vector<int> sizes;
+    char maze[MAZE_HEIGHT][MAZE_WIDTH];
 
     while(getline(data, line)) {
         std::istringstream iss(line);
         string s;
 
-
         if (!(iss >> s)) { 
             break;
         }
 
-        if (s == "$") {
-            // command
-            string command;
-            iss >> command;
-
-            if (command == "ls") {
-                // do nothing
-            } else { // cd
-                string dir_path;
-                iss >> dir_path;
-
-                if (dir_path == "/") {
-                    current_dir = root;
-                } else if (dir_path == "..") {
-                    current_dir = current_dir->parent;
-                } else {
-                    current_dir = current_dir->get_child(dir_path);
-                }
-            }
-        } else {
-            int size;
-            string name;
-
-            // check if item is numeral
-            if (s[0] >= '0' && s[0] <= '9') {
-                // file
-                size = stoi(s);
-                iss >> name;
-                current_dir->add_file(size);
-            } else {
-                // directory
-                iss >> name;
-                current_dir->add_child(name);
-            }
-        }
-
+        // do stuff
     }
-
-    root->set_total_size();
-    // do stuff
-    root->add_size(sizes);
-
-    int sum = 0;
-    for (int size : sizes) {
-        sum += size;
-    }
-    cout << sum << endl;
-
     return 0;
 }
---
+++ puzzles/7_2.cc
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Directory {
+    string name;
+    int files_size;
+    vector<Directory*> children;
+    Directory* parent;
+
+    int total_size;
+
+    Directory(string name, Directory* parent) {
+        this->name = name;
+        files_size = 0;
+        this->parent = parent;
+    }
+
+    void add_child(string name) {
+        if (child_exists(name)) {
+            return;
+        }
+        Directory* child = new Directory(name, this);
+        children.push_back(child);
+    }
+
+    bool child_exists(string name) {
+        for (Directory* child : children) {
+            if (child->name == name) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void add_file(int size) {
+        files_size += size;
+    }
+
+    Directory* get_child(string name) {
+        for (Directory* child : children) {
+            if (child->name == name) {
+                return child;
+            }
+        }
+        return nullptr;
+    }
+
+    int set_total_size() {
+        int total = files_size;
+        for (Directory* child : children) {
+            total += child->set_total_size();
+        }
+        total_size = total;
+        return total;
+    }
+
+    void add_size(vector<int>& sizes) {
+
+        sizes.push_back(total_size);
+        
+        for (Directory* child : children) {
+            child->add_size(sizes);
+        }
+    }
+};
+
+
+
+int main() {
+    fstream data("../inputs/7.txt", ios::in);
+    
+    string line;
+
+    Directory* root = new Directory("/", nullptr);
+    Directory* current_dir = root;
+
+    vector<int> sizes;
+
+    while(getline(data, line)) {
+        std::istringstream iss(line);
+        string s;
+
+
+        if (!(iss >> s)) { 
+            break;
+        }
+
+        if (s == "$") {
+            // command
+            string command;
+            iss >> command;
+
+            if (command == "ls") {
+                // do nothing
+            } else { // cd
+                string dir_path;
+                iss >> dir_path;
+
+                if (dir_path == "/") {
+                    current_dir = root;
+                } else if (dir_path == "..") {
+                    current_dir = current_dir->parent;
+                } else {
+                    current_dir = current_dir->get_child(dir_path);
+                }
+            }
+        } else {
+            int size;
+            string name;
+
+            // check if item is numeral
+            if (s[0] >= '0' && s[0] <= '9') {
+                // file
+                size = stoi(s);
+                iss >> name;
+                current_dir->add_file(size);
+            } else {
+                // directory
+                iss >> name;
+                current_dir->add_child(name);
+            }
+        }
+
+    }
+
+    root->set_total_size();
+    // do stuff
+    root->add_size(sizes);
+
+    auto root_size = root->total_size;
+    int to_be_free = 30000000 - (70000000 - root_size);
+
+    vector<int> candidates;
+
+    for (int i = 0; i < sizes.size(); i++) {
+        if (sizes[i] >= to_be_free) {
+            candidates.push_back(sizes[i]);
+        }
+    }
+
+    // sort candidates
+    std::sort(candidates.begin(), candidates.end());
+
+    // print candidates
+    for (int i = 0; i < candidates.size(); i++) {
+        cout << candidates[i] << endl;
+    }
+
+    return 0;
+}
