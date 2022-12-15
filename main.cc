6d50788d201875c7519d214a1a903b80555a62c5
Hackjaku <stressfest@rocketmail.com>
2022-12-15 14:36:24 +0100

    UPDATE

--- main.cc
+++ main.cc
@@ -3,12 +3,33 @@
 #include <string>
 #include <sstream>
 
+#include <boost/algorithm/string.hpp>
+
 using namespace std;
 
+#define HEIGHT 200
+#define WIDTH 550
+
 int main() {
-    fstream data("../inputs/10.txt", ios::in);
+    fstream data("14.txt", ios::in);
     
     string line;
+
+    char maze[HEIGHT][WIDTH];
+    // initialize maze with '.'
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            maze[i][j] = '.';
+        }
+    }
+
+    // // print maze
+    // for (int i = 0; i < HEIGHT; i++) {
+    //     for (int j = 0; j < WIDTH; j++) {
+    //         cout << maze[i][j];
+    //     }
+    //     cout << endl;
+    // }
 
     while(getline(data, line)) {
         std::istringstream iss(line);
@@ -18,7 +39,18 @@ int main() {
             break;
         }
 
-        // do stuff
+        vector<string> tokens;
+        boost::split(tokens, line, boost::is_any_of(" -> "), boost::token_compress_on);
+
+        for (auto token : tokens) {
+            vector<string> coords;
+            boost::split(coords, token, boost::is_any_of(","));
+            // print coords
+            for (auto coord : coords) {
+                cout << coord << endl;
+            }
+        }
     }
+
     return 0;
 }
