ff782b1b96a6b2b0783981b3b08a3eabdc0d10ae
Hackjaku <stressfest@rocketmail.com>
2022-12-19 18:05:40 +0100

    CHEAT: 19

--- main.cc
+++ main.cc
@@ -1,152 +1,79 @@
-#include <cinttypes>
-#include <fstream>
-#include <functional>
-#include <iostream>
-#include <iterator>
-#include <limits>
-#include <numeric>
-#include <set>
-#include <tuple>
-#include <vector>
-
-
-#include <istream>
 #include <regex>
-#include <string>
-#include <string_view>
-#include <cerrno>
-#include <charconv>
-#include <cstdlib>
-#include <cstring>
+#include <fstream>
+#include <iostream>
 
-int int_match(const std::smatch& m, int k);
-std::string& eatline(std::istream& is, std::string& buf);
-[[noreturn]] void die(const std::string_view msg);
+static uint32_t const ORE = 1 << 0;
+static uint32_t const CLY = 1 << 8;
+static uint32_t const OBS = 1 << 16;
+static uint32_t const GEO = 1 << 24;
 
+static inline uint32_t const ore(uint32_t data) { return (data >> 0) & 0xff; }
+static inline uint32_t const cly(uint32_t data) { return (data >> 8) & 0xff; }
+static inline uint32_t const obs(uint32_t data) { return (data >> 16) & 0xff; }
+static inline uint32_t const geo(uint32_t data) { return (data >> 24) & 0xff; }
 
+static inline uint32_t const eta(uint32_t needed, uint32_t produced) { return (needed + produced - 1) / produced; }
 
-[[noreturn]] void die(const std::string_view msg) {
-    std::cerr << msg << std::endl;
-    std::exit(1);
-}
+static uint32_t explore(uint32_t const *costs, uint32_t t, uint32_t stash = 0x0, uint32_t robots = ORE)
+{
+    // Default score if we just do nothing
+    uint32_t best = geo(stash + t * robots);
 
-std::string& eatline(std::istream& is, std::string& buf) {
-    if (!std::getline(is, buf)) die(strerror(errno));
-    return buf;
-}
-
-int int_match(const std::smatch& m, int k) {
-    int val;
-    auto result = std::from_chars<int>(&*m[k].first, &*m[k].second, val);
-    if (result.ec != std::errc()) die("bad int: " + m.str());
-    return val;
-}
-
-struct pt3 {
-    int64_t x, y, z;
-};
-std::istream& operator>>(std::istream& is, pt3& pt) {
-    char comma;
-    return is >> pt.x >> comma >> pt.y >> comma >> pt.z;
-}
-std::ostream& operator<<(std::ostream& os, const pt3& pt) {
-    return os << '[' << pt.x << ',' << pt.y << ',' << pt.z << ']';
-}
-pt3 operator+(pt3 a, pt3 b) { return {a.x + b.x, a.y + b.y, a.z + b.z}; }
-bool operator<(pt3 a, pt3 b) {
-    using tuple = std::tuple<int64_t, int64_t, int64_t>;
-    return tuple(a.x, a.y, a.z) < tuple(b.x, b.y, b.z);
-}
-
-// you should see the template and C++20 concepts goo i tried to write here
-pt3 upper_bound(const std::vector<pt3>& pts) {
-    static constexpr int64_t kMin = std::numeric_limits<int64_t>::min();
-    pt3 ub{kMin, kMin, kMin};
-    for (pt3 pt : pts) {
-        ub.x = std::max(ub.x, pt.x);
-        ub.y = std::max(ub.y, pt.y);
-        ub.z = std::max(ub.z, pt.z);
+    // Try to build an ore robot (useless if we already produce enough for any kind of robot)
+    if (ore(robots) < costs[2] || ore(robots) < costs[3] || ore(robots) < costs[5])
+    {
+        uint32_t dt = 1 + (ore(stash) > costs[1] ? 0 : eta(costs[1] - ore(stash), ore(robots)));
+        if (t > dt)
+            best = std::max(best, explore(costs, t - dt, stash + dt * robots - costs[1] * ORE, robots + ORE));
     }
-    return ub;
-}
 
-using grid = std::vector<std::vector<std::vector<bool>>>;
-grid empty(size_t dx, size_t dy, size_t dz) {
-    return std::vector(dx, std::vector(dy, std::vector<bool>(dz, false)));
-}
-grid grid_of(const std::vector<pt3>& pts) {
-    auto [x, y, z] = upper_bound(pts);
-    grid g = empty(x + 1, y + 1, z + 1);
-    for (pt3 pt : pts) g[pt.x][pt.y][pt.z] = true;
-    return g;
-}
-bool in_bounds(const grid& g, pt3 pt) {
-    if (pt.x < 0 || pt.y < 0 || pt.z < 0) return false;
-    if (pt.x >= g.size() || pt.y >= g[0].size() || pt.z >= g[0][0].size())
-        return false;
-    return true;
-}
-bool has(const grid& g, pt3 pt) {
-    return in_bounds(g, pt) && g[pt.x][pt.y][pt.z];
-}
-
-static constexpr pt3 kDirs[] = {
-    {-1, 0, 0}, {1, 0, 0}, {0, -1, 0}, {0, 1, 0}, {0, 0, -1}, {0, 0, 1},
-};
-
-int64_t surface_area(pt3 pt, std::function<bool(pt3)> count_nbr) {
-    int64_t area = 6;
-    for (int dir = 0; dir < 6; dir++) {
-        pt3 nbr = pt + kDirs[dir];
-        if (!count_nbr(nbr)) area -= 1;
+    // Try to build a clay robot (useless if we already produce enough for an obsidian robot)
+    if (cly(robots) < costs[4])
+    {
+        uint32_t dt = 1 + (ore(stash) > costs[2] ? 0 : eta(costs[2] - ore(stash), ore(robots)));
+        if (t > dt)
+            best = std::max(best, explore(costs, t - dt, stash + dt * robots - costs[2] * ORE, robots + CLY));
     }
-    return area;
-}
 
-int64_t droplet_area(const grid& g, std::function<bool(pt3)> count_nbr) {
-    int64_t sum = 0;
-    for (int64_t x = 0; x < g.size(); x++) {
-        for (int64_t y = 0; y < g[0].size(); y++) {
-            for (int64_t z = 0; z < g[0][0].size(); z++) {
-                pt3 pt{x, y, z};
-                if (has(g, pt)) sum += surface_area(pt, count_nbr);
-            }
-        }
+    // Try to build an obsidian robot (impossible if we don’t have a clay robot)
+    if (cly(robots))
+    {
+        uint32_t dt = 1 + std::max(ore(stash) > costs[3] ? 0 : eta(costs[3] - ore(stash), ore(robots)),
+                                cly(stash) > costs[4] ? 0 : eta(costs[4] - cly(stash), cly(robots)));
+        if (t > dt)
+            best = std::max(best, explore(costs, t - dt, stash + dt * robots - costs[3] * ORE - costs[4] * CLY, robots + OBS));
     }
-    return sum;
-}
 
-void explore_exterior(grid& exterior, const grid& g, std::set<pt3>& v, pt3 pt) {
-    v.insert(pt);
-    exterior[pt.x][pt.y][pt.z] = true;
-    for (int dir = 0; dir < 6; dir++) {
-        pt3 nbr = pt + kDirs[dir];
-        if (v.count(nbr) || !in_bounds(exterior, nbr) || has(g, nbr)) continue;
-        explore_exterior(exterior, g, v, nbr);
+    // Try to build a geode robot (impossible if we don’t have an obsidian robot)
+    if (obs(robots))
+    {
+        uint32_t dt = 1 + std::max(ore(stash) > costs[5] ? 0 : eta(costs[5] - ore(stash), ore(robots)),
+                                obs(stash) > costs[6] ? 0 : eta(costs[6] - obs(stash), obs(robots)));
+        if (t > dt)
+            best = std::max(best, explore(costs, t - dt, stash + dt * robots - costs[5] * ORE - costs[6] * OBS, robots + GEO));
     }
+
+    return best;
 }
 
-grid exterior_of(const grid& g, const std::vector<pt3>& pts) {
-    pt3 ub = upper_bound(pts);
-    grid exterior = empty(ub.x + 2, ub.y + 2, ub.z + 2);
-    std::set<pt3> v;
-    explore_exterior(exterior, g, v, ub + pt3{1, 1, 1});
-    return exterior;
-}
+int main(void)
+{
+    std::vector<uint32_t> data;
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) die("usage: day18 <file>");
-    std::ifstream ifs(argv[1]);
-    std::istream_iterator<pt3> begin(ifs), end;
-    std::vector<pt3> pts(begin, end);
+    std::ifstream t("../inputs/19.txt");
+    std::string input((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+    std::regex const r("([0-9]+)");
+    std::sregex_iterator matches(input.begin(), input.end(), r), end;
+    for (auto it = matches; it != end; ++it)
+        data.push_back(std::atoi((*it).str().c_str()));
 
-    grid droplet = grid_of(pts);
-    std::cout << droplet_area(droplet, [&](pt3 pt) {
-        return !has(droplet, pt);
-    }) << std::endl;
+    uint32_t p1 = 0;
+    for (size_t i = 0; i < data.size(); i += 7)
+        p1 += data[i] * explore(&data.at(i), 24);
+    std::cout << p1 << '\n';
 
-    grid exterior = exterior_of(droplet, pts);
-    std::cout << droplet_area(droplet, [&](pt3 pt) {
-        return has(exterior, pt) || !in_bounds(droplet, pt);
-    }) << std::endl;
+    uint32_t p2 = 1;
+    for (size_t i = 0; i < 3 * 7; i += 7)
+        p2 *= explore(&data.at(i), 32);
+    std::cout << p2 << '\n';
 }
