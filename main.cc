8a0c29076cf66bc776351c30cd363e542719162d
Hackjaku <stressfest@rocketmail.com>
2022-12-19 17:48:26 +0100

    CHEAT: 18

--- main.cc
+++ main.cc
@@ -1,169 +1,152 @@
-
-#include <algorithm>
-#include <array>
-#include <boost/tokenizer.hpp>
-#include <cstddef>
-#include <cassert>
-#include <deque>
+#include <cinttypes>
 #include <fstream>
 #include <functional>
 #include <iostream>
-#include <regex>
-#include <map>
-#include <memory>
+#include <iterator>
+#include <limits>
 #include <numeric>
-#include <optional>
 #include <set>
-#include <sstream>
-#include <stdexcept>
-#include <string_view>
 #include <tuple>
-#include <cassert>
+#include <vector>
 
-// Types
-using Coordinate = std::pair<std::int32_t, std::int32_t>;
-using Shape = std::set<Coordinate>;
-using Level = std::bitset<7>;
-using Field = std::vector<Level>;
 
-// Shapes
-const static auto s_Shapes = std::array<Shape, 5> {{
-    { Coordinate { 3, 3 }, Coordinate { 4, 3 }, Coordinate { 5, 3 }, Coordinate { 6, 3 } },                      // "-"
-    { Coordinate { 4, 5 }, Coordinate { 3, 4 }, Coordinate { 4, 4 }, Coordinate { 5, 4 }, Coordinate { 4, 3 } }, // "+"
-    { Coordinate { 3, 3 }, Coordinate { 4, 3 }, Coordinate { 5, 3 }, Coordinate { 5, 4 }, Coordinate { 5, 5 } }, // "mirrored L"
-    { Coordinate { 3, 3 }, Coordinate { 3, 4 }, Coordinate { 3, 5 }, Coordinate { 3, 6 } },                      // "I"
-    { Coordinate { 3, 3 }, Coordinate { 4, 3 }, Coordinate { 3, 4 }, Coordinate { 4, 4 } }                       // square
-}};
+#include <istream>
+#include <regex>
+#include <string>
+#include <string_view>
+#include <cerrno>
+#include <charconv>
+#include <cstdlib>
+#include <cstring>
 
-// Used to add the global initial height, or decrease the height (by adding "-1")
-auto operator+(const Shape& a_Shape, std::int32_t a_YPos) noexcept {
-    auto l_NewShape = Shape {};
-    for (const auto& l_Coordinate: a_Shape) {
-        const auto& [l_X, l_Y] = l_Coordinate;
-        l_NewShape.insert(Coordinate { l_X, (l_Y + a_YPos) } );
-    } // for
-    
-    return l_NewShape;
+int int_match(const std::smatch& m, int k);
+std::string& eatline(std::istream& is, std::string& buf);
+[[noreturn]] void die(const std::string_view msg);
+
+
+
+[[noreturn]] void die(const std::string_view msg) {
+    std::cerr << msg << std::endl;
+    std::exit(1);
 }
 
-// Move a piece left or right
-[[nodiscard]] auto BlowPiece(Shape a_Piece, bool a_bToLeft) noexcept {
-    auto l_MovedPiece = Shape {};
-    for (const auto& l_Coordinate: a_Piece) {
-        const auto& [l_X, l_Y] = l_Coordinate;
-        auto l_XDelta = (a_bToLeft) ? -1 : 1;
-        l_MovedPiece.insert(Coordinate { (l_X + l_XDelta), l_Y } );
-    } // for
-    
-    return l_MovedPiece; // may return an out-of-bounds piece!
+std::string& eatline(std::istream& is, std::string& buf) {
+    if (!std::getline(is, buf)) die(strerror(errno));
+    return buf;
 }
 
-// Check if a given piece fits into the playing field
-bool Fits(const Field& a_Field, const Shape& a_Piece) noexcept {
-    for (const auto& l_Coordinate: a_Piece) {
-        const auto& [l_X, l_Y] = l_Coordinate;
-        if ((l_X < 1) || (l_X > 7)) return false; // out of bounds
-        if (l_Y < 0) return false; // below base line
-        if (l_Y < a_Field.size()) {
-            const auto& l_Line = a_Field[l_Y];
-            if (l_Line.test(l_X - 1)) {
-                return false; // collision with an already settled piece
-            } // if
-        } // if
-    } // for
-    
+int int_match(const std::smatch& m, int k) {
+    int val;
+    auto result = std::from_chars<int>(&*m[k].first, &*m[k].second, val);
+    if (result.ec != std::errc()) die("bad int: " + m.str());
+    return val;
+}
+
+struct pt3 {
+    int64_t x, y, z;
+};
+std::istream& operator>>(std::istream& is, pt3& pt) {
+    char comma;
+    return is >> pt.x >> comma >> pt.y >> comma >> pt.z;
+}
+std::ostream& operator<<(std::ostream& os, const pt3& pt) {
+    return os << '[' << pt.x << ',' << pt.y << ',' << pt.z << ']';
+}
+pt3 operator+(pt3 a, pt3 b) { return {a.x + b.x, a.y + b.y, a.z + b.z}; }
+bool operator<(pt3 a, pt3 b) {
+    using tuple = std::tuple<int64_t, int64_t, int64_t>;
+    return tuple(a.x, a.y, a.z) < tuple(b.x, b.y, b.z);
+}
+
+// you should see the template and C++20 concepts goo i tried to write here
+pt3 upper_bound(const std::vector<pt3>& pts) {
+    static constexpr int64_t kMin = std::numeric_limits<int64_t>::min();
+    pt3 ub{kMin, kMin, kMin};
+    for (pt3 pt : pts) {
+        ub.x = std::max(ub.x, pt.x);
+        ub.y = std::max(ub.y, pt.y);
+        ub.z = std::max(ub.z, pt.z);
+    }
+    return ub;
+}
+
+using grid = std::vector<std::vector<std::vector<bool>>>;
+grid empty(size_t dx, size_t dy, size_t dz) {
+    return std::vector(dx, std::vector(dy, std::vector<bool>(dz, false)));
+}
+grid grid_of(const std::vector<pt3>& pts) {
+    auto [x, y, z] = upper_bound(pts);
+    grid g = empty(x + 1, y + 1, z + 1);
+    for (pt3 pt : pts) g[pt.x][pt.y][pt.z] = true;
+    return g;
+}
+bool in_bounds(const grid& g, pt3 pt) {
+    if (pt.x < 0 || pt.y < 0 || pt.z < 0) return false;
+    if (pt.x >= g.size() || pt.y >= g[0].size() || pt.z >= g[0][0].size())
+        return false;
     return true;
 }
-
-// Permanently add a piece, which moved to its final position, to the playing field
-void Settle(Field& a_Field, const Shape& a_Piece) noexcept {
-    for (const auto& l_Coordinate: a_Piece) {
-        const auto& [l_X, l_Y] = l_Coordinate;
-        while (a_Field.size() <= l_Y) {
-            a_Field.push_back({});
-        } // while
-        
-        a_Field[l_Y].set(l_X - 1);
-    } // for
+bool has(const grid& g, pt3 pt) {
+    return in_bounds(g, pt) && g[pt.x][pt.y][pt.z];
 }
 
-void Play(std::string_view a_FileName) noexcept {
-    // Read wind movement information
-    std::cout << std::endl << "Reading wind movement information from file \"" << a_FileName << "\"" << std::endl;
-    std::ifstream l_InputStream { std::string { a_FileName } };
-    std::string l_WindData;
-    const auto l_ReadResult = static_cast<bool>(std::getline(l_InputStream, l_WindData));
-    assert(l_ReadResult);
-    
-    auto l_Field = Field{};
-    auto l_FieldDubDet = Field{};
-    
-    auto l_HeightA = std::uint64_t { 0 };
-    
-    const auto l_DetectorMaxWindow = 10000;
-    auto l_DetectedCycle = std::uint64_t { 0 };
-    auto l_DetectedCycleMod = std::uint64_t { 0 };
-    
-    auto l_DeltaSteps = std::uint64_t { 0 };
-    auto l_DeltaSize1 = std::uint64_t { 0 };
-    auto l_DeltaSize2 = std::uint64_t { 0 };
-    
-    auto l_WindIndex = 0;
-    auto l_ShapeIndex = 0;
-    auto l_PiecesDropped = std::uint64_t { 0 };
-    while (l_PiecesDropped < (3 * l_DetectorMaxWindow)) { // wait one window for settlement, then two more for maximum matching
-        if (l_PiecesDropped == 2022) {
-            l_HeightA = l_Field.size();
-        } else if (l_PiecesDropped == (2 * l_DetectorMaxWindow)) {
-            l_FieldDubDet = l_Field;
-        } else if ((!l_DetectedCycle) && (l_PiecesDropped > (2 * l_DetectorMaxWindow))) {
-            if (std::memcmp(&l_Field[l_Field.size() - l_DetectorMaxWindow],
-                            &l_FieldDubDet[l_FieldDubDet.size() - l_DetectorMaxWindow],
-                            l_DetectorMaxWindow * sizeof(Level)) == 0) {
-                l_DetectedCycle = (l_PiecesDropped - (2 * l_DetectorMaxWindow));
-                l_DetectedCycleMod = (1000000000000 % l_DetectedCycle);
-            } // if
-        } else if (l_DetectedCycle && (!l_DeltaSize1) && ((l_PiecesDropped % l_DetectedCycle) == l_DetectedCycleMod)) {
-            // Cycle known
-            l_DeltaSize1 = l_Field.size();
-        } else if (l_DetectedCycle && l_DeltaSize1 && (!l_DeltaSteps) && ((l_PiecesDropped % l_DetectedCycle) == l_DetectedCycleMod)) {
-            l_DeltaSteps = l_PiecesDropped;
-            l_DeltaSize2 = l_Field.size();
-            break;
-        } // else if
-        
-        auto l_Piece1 = s_Shapes[l_ShapeIndex] + l_Field.size(); // Spawn new piece
-        assert(Fits(l_Field, l_Piece1));
-        
-        while (true) {
-            // Manipulate one falling object
-            const auto l_Piece2 = BlowPiece(l_Piece1, l_WindData[l_WindIndex] == '<');
-            const auto l_Piece3 = Fits(l_Field, l_Piece2) ? l_Piece2 : l_Piece1;
-            const auto l_Piece4 = (l_Piece3 + (-1));            
-            if ((++l_WindIndex) == l_WindData.size()) {
-                l_WindIndex = 0;
-            } // if
-        
-            if (!Fits(l_Field, l_Piece4)) {
-                Settle(l_Field, l_Piece3);
-                break;
-            } else  {
-                l_Piece1 = l_Piece4;
-            } // else
-        } // while
-    
-        if (++l_ShapeIndex == s_Shapes.size()) {
-            l_ShapeIndex = 0;
-        } // if
-        
-        ++l_PiecesDropped;
-    } // while
-    
-    std::cout << "A: height = " << l_HeightA << std::endl;
-    std::cout << "B: height = " << ((((1000000000000 - l_DeltaSteps) / l_DetectedCycle) * (l_DeltaSize2 - l_DeltaSize1)) + l_DeltaSize2) << std::endl;
+static constexpr pt3 kDirs[] = {
+    {-1, 0, 0}, {1, 0, 0}, {0, -1, 0}, {0, 1, 0}, {0, 0, -1}, {0, 0, 1},
+};
+
+int64_t surface_area(pt3 pt, std::function<bool(pt3)> count_nbr) {
+    int64_t area = 6;
+    for (int dir = 0; dir < 6; dir++) {
+        pt3 nbr = pt + kDirs[dir];
+        if (!count_nbr(nbr)) area -= 1;
+    }
+    return area;
 }
 
-int main(int argc, char **argv) {
-    Play("../inputs/17_test.txt");
-    Play("../inputs/17.txt");
+int64_t droplet_area(const grid& g, std::function<bool(pt3)> count_nbr) {
+    int64_t sum = 0;
+    for (int64_t x = 0; x < g.size(); x++) {
+        for (int64_t y = 0; y < g[0].size(); y++) {
+            for (int64_t z = 0; z < g[0][0].size(); z++) {
+                pt3 pt{x, y, z};
+                if (has(g, pt)) sum += surface_area(pt, count_nbr);
+            }
+        }
+    }
+    return sum;
+}
+
+void explore_exterior(grid& exterior, const grid& g, std::set<pt3>& v, pt3 pt) {
+    v.insert(pt);
+    exterior[pt.x][pt.y][pt.z] = true;
+    for (int dir = 0; dir < 6; dir++) {
+        pt3 nbr = pt + kDirs[dir];
+        if (v.count(nbr) || !in_bounds(exterior, nbr) || has(g, nbr)) continue;
+        explore_exterior(exterior, g, v, nbr);
+    }
+}
+
+grid exterior_of(const grid& g, const std::vector<pt3>& pts) {
+    pt3 ub = upper_bound(pts);
+    grid exterior = empty(ub.x + 2, ub.y + 2, ub.z + 2);
+    std::set<pt3> v;
+    explore_exterior(exterior, g, v, ub + pt3{1, 1, 1});
+    return exterior;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) die("usage: day18 <file>");
+    std::ifstream ifs(argv[1]);
+    std::istream_iterator<pt3> begin(ifs), end;
+    std::vector<pt3> pts(begin, end);
+
+    grid droplet = grid_of(pts);
+    std::cout << droplet_area(droplet, [&](pt3 pt) {
+        return !has(droplet, pt);
+    }) << std::endl;
+
+    grid exterior = exterior_of(droplet, pts);
+    std::cout << droplet_area(droplet, [&](pt3 pt) {
+        return has(exterior, pt) || !in_bounds(droplet, pt);
+    }) << std::endl;
 }
